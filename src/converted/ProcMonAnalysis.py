3a8b32a62d4ab223ecad8d76a9191cb8bece4478
Jonathan Evenari <jonathan.evenari@anybill.de>
2022-05-03 09:39:22 +0200

    Extended the analysis by folders created/modified/deleted and by reg keys/values created/modified/deleted.

--- src/converted/ProcMonAnalysis.py
+++ src/converted/ProcMonAnalysis.py
@@ -3,7 +3,7 @@
 
 # # Imports
 
-# In[21]:
+# In[1]:
 
 
 import json
@@ -14,13 +14,19 @@ import pandas as pd
 
 # ## Paths
 
-# In[12]:
+# In[2]:
 
 
 current_malware = "Cerber"
 
 
-# In[13]:
+# In[3]:
+
+
+project_root = "/home/jevenari/PycharmProjects/ForensicalAnalysis"
+
+
+# In[4]:
 
 
 config_path = "/home/jevenari/PycharmProjects/ForensicalAnalysis/config/config.json"
@@ -28,40 +34,38 @@ config = json.load(open(config_path, "r"))
 config = config[current_malware]
 
 
-# In[15]:
+# In[5]:
 
 
 procmon_path = f"{config['Dynamic']}/{config['ProcessMonitor']}"
 
 
+# In[6]:
+
+
+regshot_path = f"{project_root}/data/{current_malware}/{config['Regshot']}"
+
+
 # ## Pandas
 
-# In[39]:
+# In[7]:
 
 
 pd.set_option('display.min_rows', 1000)
 pd.set_option("display.max_rows", 10000)
 
 
-# # Code
+# # Process Monitor Analysis
 
-# In[ ]:
+# ## Load data
 
-
-
-
-
-# # Analysis
-
-# ## Load ProccessMonitor data
-
-# In[43]:
+# In[8]:
 
 
 df_procmon = pd.read_csv(procmon_path)
 
 
-# In[60]:
+# In[9]:
 
 
 df_procmon
@@ -69,7 +73,7 @@ df_procmon
 
 # ## Get unique operations
 
-# In[64]:
+# In[13]:
 
 
 sorted(list(df_procmon["Operation"].unique()))
@@ -77,63 +81,95 @@ sorted(list(df_procmon["Operation"].unique()))
 
 # ## Get Process Create/Process Exit/Process Start events)
 
-# In[69]:
+# In[14]:
 
 
 df_process_create = df_procmon.query("Operation == 'Process Create'")
 
 
-# In[70]:
+# In[15]:
 
 
 df_process_create
 
 
-# In[73]:
+# In[16]:
 
 
 df_process_start = df_procmon.query("Operation == 'Process Start'")
 
 
-# In[74]:
+# In[17]:
 
 
 df_process_start
 
 
-# In[71]:
+# In[18]:
 
 
 df_process_exit = df_procmon.query("Operation == 'Process Exit'")
 
 
-# In[72]:
+# In[19]:
 
 
 df_process_exit
 
 
+# ## Get Thread Create/Thread Exit
+
+# In[20]:
+
+
+df_thread_create = df_procmon.query("Operation == 'Thread Create'")
+
+
+# In[21]:
+
+
+df_thread_create
+
+
+# In[22]:
+
+
+df_thread_exit = df_procmon.query("Operation == 'Thread Exit'")
+
+
+# In[23]:
+
+
+df_thread_exit
+
+
 # ## Get RegCreateKey/RegSetValue data
 
-# In[67]:
+# In[24]:
 
 
 df_reg_value_set = df_procmon.query("Operation == 'RegCreateKey'")
 
 
-# In[68]:
+# In[25]:
 
 
 df_reg_value_set
 
 
-# In[65]:
+# In[26]:
 
 
 df_reg_value_set = df_procmon.query("Operation == 'RegSetValue'")
 
 
-# In[66]:
+# In[27]:
+
+
+df_reg_value_set.count()
+
+
+# In[28]:
 
 
 df_reg_value_set
@@ -141,32 +177,291 @@ df_reg_value_set
 
 # ## Get loaded DLLs
 
-# In[42]:
+# In[29]:
 
 
 df_loaded_dlls = df_procmon.query("Operation == 'Load Image'")
 
 
-# In[50]:
+# In[30]:
 
 
 unique_dlls = pd.unique(df_loaded_dlls["Path"])
 
 
-# In[52]:
+# In[31]:
 
 
 df_unique_dlls = pd.DataFrame(unique_dlls, columns=["Path"])
 
 
-# In[57]:
+# In[32]:
 
 
 df_unique_dlls["DLL"] = df_unique_dlls["Path"].apply(lambda path: path.split("\\")[-1])
 
 
-# In[59]:
+# In[33]:
 
 
 df_unique_dlls
+
+
+# # Regshot Analysis
+
+# ## Load data
+
+# In[42]:
+
+
+df_regshot_data = pd.read_csv(regshot_path, delimiter=";")
+
+
+# ## Show unique types & operations
+
+# In[50]:
+
+
+sorted(df_regshot_data["Type"].unique())
+
+
+# In[51]:
+
+
+sorted(df_regshot_data["Operation"].unique())
+
+
+# ## Files Created
+
+# In[46]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'File' & Operation == 'Deleted'")
+
+
+# In[47]:
+
+
+df_operation_query
+
+
+# In[49]:
+
+
+df_operation_query.count()
+
+
+# ## Files modified
+
+# In[52]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'File' & Operation == 'Modified'")
+
+
+# In[53]:
+
+
+df_operation_query
+
+
+# In[54]:
+
+
+df_operation_query.count()
+
+
+# ## Files Deleted
+
+# In[79]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'File' & Operation == 'Deleted'")
+
+
+# In[80]:
+
+
+df_operation_query
+
+
+# In[81]:
+
+
+df_operation_query.count()
+
+
+# ## Folders Created
+
+# In[58]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Folder' & Operation == 'Deleted'")
+
+
+# In[59]:
+
+
+df_operation_query
+
+
+# In[60]:
+
+
+df_operation_query.count()
+
+
+# ## Folders modified
+
+# In[61]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Folder' & Operation == 'Modified'")
+
+
+# In[62]:
+
+
+df_operation_query
+
+
+# In[64]:
+
+
+df_operation_query.count()
+
+
+# ## Folders Deleted
+
+# In[76]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Folder' & Operation == 'Deleted'")
+
+
+# In[77]:
+
+
+df_operation_query
+
+
+# In[78]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Keys Created
+
+# In[67]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Key' & Operation == 'Deleted'")
+
+
+# In[68]:
+
+
+df_operation_query
+
+
+# In[69]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Keys modified
+
+# In[70]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Key' & Operation == 'Modified'")
+
+
+# In[71]:
+
+
+df_operation_query
+
+
+# In[72]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Keys Deleted
+
+# In[73]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Value' & Operation == 'Deleted'")
+
+
+# In[75]:
+
+
+df_operation_query
+
+
+# In[74]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Values Created
+# <h4 style="color: red">CAUTION: This part could not be parsed correctly, since the actual values were distributed over multiple lines resulting in a random pattern, that was impossible to parse.</h4>
+
+# In[46]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Value' & Operation == 'Deleted'")
+
+
+# In[47]:
+
+
+df_operation_query
+
+
+# In[49]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Values modified
+
+# In[52]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Value' & Operation == 'Modified'")
+
+
+# In[53]:
+
+
+df_operation_query
+
+
+# In[54]:
+
+
+df_operation_query.count()
+
+
+# ## Registry Values Deleted
+
+# In[55]:
+
+
+df_operation_query = df_regshot_data.query("Type == 'Value' & Operation == 'Deleted'")
+
+
+# In[56]:
+
+
+df_operation_query.count()
 
